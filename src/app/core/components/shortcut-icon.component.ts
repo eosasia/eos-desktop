d42b3d25e1f2aad6802b7a6347230f302e82e531
Errick Benson Peart <errickpeart@gmail.com>
2018-09-08 15:43:14 -0400

    add icon basic context menu

--- src/app/core/components/shortcut-icon.component.ts
+++ src/app/core/components/shortcut-icon.component.ts
@@ -1,20 +1,38 @@
-import {Component, ElementRef, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, Input, OnInit} from '@angular/core';
 import {WindowsService} from '../../windows/services/windows.services';
 import {AppService} from '../services/app.service';
 
 @Component({
   selector: 'app-shortcut-icon',
+  styleUrls: ['../styles/shortcut-icon.css'],
   templateUrl: '../templates/shortcut-icon.component.html',
 })
 export class ShortcutIconComponent implements OnInit {
   topDistance: string;
   leftDistance: string;
   @Input() info: any;
+  isContextMenuVisible = false;
+  rightPanelStyle: Object = {};
 
   constructor(private el: ElementRef, private windowsSvc: WindowsService, private appSvc: AppService) {}
 
   ngOnInit() {
     this._setIconPosition();
+  }
+
+  @HostListener('contextmenu', ['$event'])
+  onClick(event: MouseEvent) {
+    event.preventDefault();
+    this.rightPanelStyle = {'left': event.clientX + 'px', 'top': event.clientY + 'px'};
+    this.isContextMenuVisible = !this.isContextMenuVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  hideContextMenu(event: MouseEvent) {
+    event.preventDefault();
+    if (this.isContextMenuVisible) {
+      this.isContextMenuVisible = false;
+    }
   }
 
   private _setIconPosition() {
@@ -28,5 +46,4 @@ export class ShortcutIconComponent implements OnInit {
   openWindow() {
     this.windowsSvc.addWindow(this.info);
   }
-
 }
